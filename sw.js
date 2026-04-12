5d111edb24786c1f21f9c8137b9565eb0027eeaf
Youhei.Yamada <yamada@wainoMacBook-Air.local>
2026-04-12 19:22:55 +0900

    initial commit

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_VERSION = 'v1.0.0';
+const CACHE_NAME = `teacher-tasks-${CACHE_VERSION}`;
+
+const CACHE_URLS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return Promise.allSettled(
+        CACHE_URLS.map(url => cache.add(url).catch(() => null))
+      );
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request).then((response) => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
